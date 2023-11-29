f99ea6caf799c545ee0349ca7e082e7cccdccdb3
ljx <lijiaxing@mail.ustc.edu.cn>
2023-11-29 19:47:37 +0800

    implement cglib instantiation strategy

--- src/main/java/com/ljx/springframework/beans/factory/support/CglibSubclassingInstantiationStrategy.java
+++ src/main/java/com/ljx/springframework/beans/factory/support/CglibSubclassingInstantiationStrategy.java
@@ -2,6 +2,8 @@ package com.ljx.springframework.beans.factory.support;
 
 import com.ljx.springframework.beans.BeansException;
 import com.ljx.springframework.beans.factory.config.BeanDefinition;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
 
 /**
  * @Author: ljx
@@ -11,6 +13,9 @@ import com.ljx.springframework.beans.factory.config.BeanDefinition;
 public class CglibSubclassingInstantiationStrategy implements InstantiationStrategy{
     @Override
     public Object instantiate(BeanDefinition beanDefinition) throws BeansException {
-        throw new UnsupportedOperationException("CGLIB instantiation strategy is not supported");
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(beanDefinition.getBeanClass());
+        enhancer.setCallback((MethodInterceptor) (obj, method, argsTemp, proxy) -> proxy.invokeSuper(obj,argsTemp));
+        return enhancer.create();
     }
 }
